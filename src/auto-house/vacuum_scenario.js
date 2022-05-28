bdc1caf931f6365dfd803ecb50d6fe661465f330
Momofil31 <filippomomesso@gmail.com>
2022-05-28 21:45:02 +0200

    :sparkles: Update vacuum scenario to work with mopbot

--- src/auto-house/vacuum_scenario.js
+++ src/auto-house/vacuum_scenario.js
@@ -1,77 +1,83 @@
+// Utility
 const Clock = require("../utils/Clock");
+const { Postman, PostmanAcceptAllRequest } = require("./helpers/Communication");
+const { Move, Charge, Suck, Clean } = require("./agents/pddlActions");
 const {
-    VacuumCleaner,
+    MopCleaningProcedureIntention,
+    AskHouseConfigurationIntention,
+    AskRoomStatusIntention,
+    AskToCleanIntention,
+    SendRoomStateIntention,
     LearnHouseGoal,
     LearnHouseIntention,
+    VacuumCleaningProcedureGoal,
+    VacuumCleaningProcedureIntention,
+    SendHouseConfigurationIntention,
+} = require("./agents/AgentIntentions");
+// Devices
+const { House } = require("./House");
+const { VacuumCleaner } = require("./devices/VacuumCleaner");
+const { MopBot } = require("./devices/MopBot");
+// Agents
+const { HouseAgent } = require("./agents/HouseAgent");
+const { VacuumCleanerAgent } = require("./agents/VacuumCleanerAgent");
+const { MopBotAgent } = require("./agents/MopBotAgent");
+
+// Plannning
+let { OnlinePlanning: VacuumCleanerPlanning } = require("../pddl/OnlinePlanner")([
     Move,
     Suck,
     Charge,
-    AskRoomStatusGoal,
-    AskRoomStatusIntention,
-} = require("./devices/VacuumCleaner");
-const { House } = require("./House");
-const { HouseAgent, SendRoomStateIntention } = require("./agents/HouseAgent");
-const { VacuumCleanerAgent } = require("./agents/VacuumCleanerAgent");
-const PlanningGoal = require("../pddl/PlanningGoal");
-const { TryCleanProcedureGoal, TryCleanProcedureIntention } = require("./helpers/RetryGoal");
-const { Postman, PostmanAcceptAllRequest } = require("./helpers/Communication");
-let { OnlinePlanning } = require("../pddl/OnlinePlanner")([Move, Suck, Charge]);
+]);
+let { OnlinePlanning: MopBotPlanning } = require("../pddl/OnlinePlanner")([Move, Clean, Charge]);
 
 global.deviceNextId = 0;
 
-// House, which includes rooms and devices
 let house = new House();
 
-// Agents
+// HOUSE AGENT
 let houseAgent = new HouseAgent("house_agent", house);
 houseAgent.intentions.push(PostmanAcceptAllRequest);
 houseAgent.intentions.push(SendRoomStateIntention);
 houseAgent.postSubGoal(new Postman());
 
-let vacuumCleanerDevice = new VacuumCleaner(house, "vacuum", "kitchen");
-let vacuumCleanerAgent = new VacuumCleanerAgent("vacuum_cleaner", vacuumCleanerDevice);
+// MOP BOT AGENT
+let mopBotDevice = new MopBot(house, "mopbot", "kitchen", "kitchen");
+let mopBotAgent = new MopBotAgent("mopbot", mopBotDevice);
 
-vacuumCleanerAgent.intentions.push(OnlinePlanning);
-vacuumCleanerAgent.intentions.push(TryCleanProcedureIntention);
-vacuumCleanerAgent.intentions.push(LearnHouseIntention);
-vacuumCleanerAgent.intentions.push(PostmanAcceptAllRequest);
-vacuumCleanerAgent.intentions.push(AskRoomStatusIntention);
-vacuumCleanerAgent.postSubGoal(new Postman());
+mopBotAgent.beliefs.declare("in kitchen");
+mopBotAgent.beliefs.declare("full_battery");
+mopBotAgent.beliefs.undeclare("zero_battery");
+
+mopBotAgent.intentions.push(MopBotPlanning);
+mopBotAgent.intentions.push(PostmanAcceptAllRequest);
+mopBotAgent.intentions.push(MopCleaningProcedureIntention);
+mopBotAgent.intentions.push(AskHouseConfigurationIntention);
+mopBotAgent.postSubGoal(new Postman());
+
+// VACUUM CLEANER AGENT
+let vacuumCleanerDevice = new VacuumCleaner(house, "vacuum", "kitchen", "kitchen");
+let vacuumCleanerAgent = new VacuumCleanerAgent("vacuum_cleaner", vacuumCleanerDevice, mopBotAgent);
 
 // initialize belief state of vacuum cleaner agent
 vacuumCleanerAgent.beliefs.declare("in kitchen");
 vacuumCleanerAgent.beliefs.declare("full_battery");
 vacuumCleanerAgent.beliefs.undeclare("zero_battery");
 
+// Add vacuum cleaner intentions
+vacuumCleanerAgent.intentions.push(VacuumCleanerPlanning);
+vacuumCleanerAgent.intentions.push(PostmanAcceptAllRequest);
+vacuumCleanerAgent.intentions.push(VacuumCleaningProcedureIntention);
+vacuumCleanerAgent.intentions.push(LearnHouseIntention);
+vacuumCleanerAgent.intentions.push(AskRoomStatusIntention);
+vacuumCleanerAgent.intentions.push(AskToCleanIntention);
+vacuumCleanerAgent.intentions.push(SendHouseConfigurationIntention);
+vacuumCleanerAgent.postSubGoal(new Postman());
+
 const learnHouseGoal = new LearnHouseGoal({
     house: house,
     start: vacuumCleanerDevice.in_room,
 });
-
-class CleanHouseGoal extends PlanningGoal {
-    constructor(house) {
-        let goal = [];
-        for (let r of Object.values(house.rooms)) {
-            if (r.name != "out") {
-                goal.push("clean " + r.name);
-            }
-        }
-        super({ goal: goal });
-    }
-}
-
-class ChargeGoal extends PlanningGoal {
-    constructor(device) {
-        super({
-            goal: ["in " + device.chargingStationRoom, "full_battery"],
-        });
-    }
-}
-
-const cleanHouseGoal = new CleanHouseGoal(house);
-const askRoomStatusGoal = new AskRoomStatusGoal({ agent: houseAgent, house: house });
-
-const chargeGoal = new ChargeGoal(vacuumCleanerDevice);
 
 vacuumCleanerAgent.postSubGoal(learnHouseGoal);
 
@@ -84,10 +90,7 @@ Clock.global.observe("mm", async () => {
     if (time.hh == 8 && time.mm == 30) {
         if (learnHouseGoal.achieved) {
             vacuumCleanerAgent.postSubGoal(
-                new TryCleanProcedureGoal({
-                    goal: { cleanHouseGoal, askRoomStatusGoal, chargeGoal },
-                    times: 2,
-                }),
+                new VacuumCleaningProcedureGoal({ houseAgent, times: 2 }),
             );
         }
     }
