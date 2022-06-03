5265c47e1378e259bef179dfea22f2103e187b7c
Momofil31 <filippomomesso@gmail.com>
2022-06-04 00:01:14 +0200

    :rotating_light: Exclude some linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,21 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "commonjs": true,
-        "es2021": true,
-        "jest": true
+    env: {
+        node: true,
+        commonjs: true,
+        es2021: true,
+        jest: true,
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": "latest"
+    extends: "eslint:recommended",
+    parserOptions: {
+        ecmaVersion: "latest",
     },
-    "rules": {
-        "indent": [
-            "error",
-            4,
-            { SwitchCase: 1}
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+    rules: {
+        indent: ["error", 4, { SwitchCase: 1 }],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        "no-unused-vars": ["error", { args: "none" }],
+        "no-constant-condition": ["error", { checkLoops: false }],
+        "no-async-promise-executor": "off",
+    },
 };
